96cdfcbb32e043bdab8be794efb69a91b71182fb
DrewMendelow <103079010+DrewMendelow@users.noreply.github.com>
2025-01-10 18:38:55 +0000

    work for 1/10

--- HelloWorldProject/src/MyProgram.java
+++ HelloWorldProject/src/MyProgram.java
@@ -20,8 +20,94 @@ public class MyProgram {
 		try{
 			File f = new File("HelloWorldProject/src/data.txt");
 			x = new Scanner (f);
-			String name = x.nextLine();
-			System.out.println(name);
+
+			Queue<Train> carList = new LinkedList<Train>();
+			String line = x.nextLine();
+			
+			while (!line.equals("END")){
+				if (line.length() > 2 && line.substring(0,3).equals("CAR")){
+					String name = line;
+					String product = x.nextLine();
+					String origin = x.nextLine();
+					String destination = x.nextLine();
+					int weight = x.nextInt();
+					int miles = x.nextInt();
+					boolean isEngine = false;
+					Train newCar = new Train(name, product, origin, destination, weight, miles, isEngine);
+					carList.add(newCar);
+					// System.out.println(newCar.getName() + ", " + newCar.getProduct() + ", " + newCar.getOrigin() + ", " + newCar.getDestination() + ", " + newCar.getWeight() + ", " + newCar.getMiles() + ", " + newCar.getIsEngine() + ", ");
+				}
+				else if (line.length() > 2 && line.substring(0,3).equals("ENG")){
+					String name = line;
+					String destination = x.nextLine();
+					boolean isEngine = true;
+					Train newCar = new Train(name, "", "", destination, 0, 0, isEngine);
+					carList.add(newCar);
+				}
+				line = x.nextLine();
+			}
+			
+			Stack<Train> Bal = new Stack<Train>();
+			int BalWeight = 0;
+
+			Stack<Train> Char = new Stack<Train>();
+			int CharWeight = 0;
+
+			Stack<Train> Tren = new Stack<Train>();
+			int TrenWeight = 0;
+
+			Stack<Train> Other = new Stack<Train>();
+			int OtherWeight = 0;
+
+			Queue<Train> reviewList = new LinkedList<Train>();
+
+			while (carList.size() > 0){
+				Train car = carList.remove();
+				if (!car.getIsEngine()){
+					if (car.getMiles() > 700){
+						reviewList.add(car);
+					}
+					else{
+						if (car.getDestination().toLowerCase().equals("baltimore")){
+							if (BalWeight < limitTrackC){
+								Bal.push(car);
+							}
+							else{
+								//SEND OF ENG0000 for Bal
+								Bal.push(car);
+							}
+						}
+						else if (car.getDestination().toLowerCase().equals("charlotte")){
+							if (CharWeight < limitTrackB){
+								Char.push(car);
+							}
+							else{
+								//SEND OF ENG0000 for Char
+								Char.push(car);
+							}
+						}
+						else if (car.getDestination().toLowerCase().equals("trenton")){
+							if (TrenWeight < limitTrackA){
+								Tren.push(car);
+							}
+							else{
+								//SEND OF ENG0000 for Tren
+								Tren.push(car);
+							}
+						}
+						else{
+							Other.push(car);
+						}
+					}
+				}
+				else{
+					//DEPART track with car destination field
+				}
+			}
+
+			while (reviewList.size() > 0){
+				
+			}
 		}
 		catch(Exception e)
 		{
--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -1,6 +1,71 @@
 public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
+    private boolean isEngine;
+    
+    public Train(String Name, String Product, String Origin, String Destination, int Weight, int Miles, boolean IsEngine){
+        name = Name;
+        product = Product;
+        origin = Origin;
+        destination = Destination;
+        weight = Weight;
+        miles = Miles;
+        isEngine = IsEngine;
+    }
 
+    public String getName(){
+        return name;
+    }
 
+    public void setName(String val){
+        name = val;
+    }
+
+    public String getProduct(){
+        return product;
+    }
+
+    public void setProduct(String val){
+        product = val;
+    }
+
+    public String getOrigin(){
+        return origin;
+    }
+
+    public void setOrigin(String val){
+        origin = val;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public void setDestination(String val){
+        destination = val;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public void setWeight(int val){
+        weight = val;
+    }
+
+    public int getMiles(){
+        return miles;
+    }
+
+    public void setMiles(int val){
+        miles = val;
+    }
+
+    public boolean getIsEngine(){
+        return isEngine;
+    }
+
+    public void setIsEngine(boolean val){
+        isEngine = val;
+    }
 }
